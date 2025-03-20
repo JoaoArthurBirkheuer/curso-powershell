85854b623411dea65c69f0fd81be95f01ca6dffb
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-20 19:05:22 -0300

    SelectString e Operators

---
+++ scripts/Operators.ps1
@@ -0,0 +1,15 @@
+﻿# Condicionais, Aritméticos e de Atribuição
+
+# Há 13 operadores de comparação (ver documento)
+# replace MODIFICA e COMPARA
+
+# Os operadores aritméticos são os básicos...
+# Os operadores de atribuição também
+
+Clear-Host
+# Para escrever código, ';' é permitido
+$var1 = 4;
+$var2 = 3;
+4/5
+
+# Get-Service | Where-Object {$_.Status -eq "Running"}
---
+++ scripts/SelectString.ps1
@@ -0,0 +1,29 @@
+﻿# Select-String
+
+@"
+Nome: João da Silva
+Idade: 30
+Nota: 8.5
+
+Nome: Maria Oliveira
+Idade: 25
+Nota: 9.0
+
+Nome: Carlos Souza
+Idade: 28
+Nota: 7.8
+"@ | Set-Content "$PSScriptRoot\dados1.txt"
+
+# Lista todos os arquivos .txt na mesma pasta do script
+Get-ChildItem -Path . -Filter *.txt
+
+# Lê e exibe todo o conteúdo do arquivo dados.txt
+Get-Content -Path .\dados1.txt
+
+# Exibe apenas as linhas que contêm a palavra "Nota"
+Get-Content -Path .\dados1.txt | Select-String -Pattern "Nota"
+
+# Para cada arquivo .txt na pasta atual, lê seu conteúdo e exibe as linhas que contêm "Nome:"
+Get-ChildItem -Filter *.txt | ForEach-Object {
+    Get-Content -Path $_.FullName | Select-String -Pattern "Nome:"
+}
